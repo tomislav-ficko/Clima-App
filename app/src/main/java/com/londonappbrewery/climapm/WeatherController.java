a52ef57ade4a9b1e4b307cf08e5b53398812b7f3
tomislav-ficko <ficko.tomislav@gmail.com>
2020-09-13 12:40:12 +0200

    Add code to execute http request

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,72 +1,153 @@
 package com.londonappbrewery.climapm;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class WeatherController extends AppCompatActivity {
 
-    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    final int REQUEST_CODE = 123;
+    final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
     final String APP_ID = "9a31aa82b36ae80e9bba5a0528a0e179";
     // Time between location updates in milliseconds
     final long MIN_TIME = 5000;
     // Distance between location updates in meters
     final float MIN_DISTANCE = 1000;
-
-    // TODO: Set LOCATION_PROVIDER here:
-
-
+    String LOCATION_PROVIDER = LocationManager.NETWORK_PROVIDER;
 
     TextView mCityLabel;
     ImageView mWeatherImage;
     TextView mTemperatureLabel;
 
-    // TODO: Declare a LocationManager and a LocationListener here:
-
+    LocationManager mLocationManager;   // component that starts/stops location updates
+    LocationListener mLocationListener; // component that's notified when the location is changed
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_controller_layout);
 
-        // Linking the elements in the layout to Java code
         mCityLabel = (TextView) findViewById(R.id.locationTV);
         mWeatherImage = (ImageView) findViewById(R.id.weatherSymbolIV);
         mTemperatureLabel = (TextView) findViewById(R.id.tempTV);
         ImageButton changeCityButton = (ImageButton) findViewById(R.id.changeCityButton);
 
+        changeCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-        // TODO: Add an OnClickListener to the changeCityButton here:
-
+            }
+        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    // TODO: Add onResume() here:
+        getWeatherForCurrentLocation();
+    }
 
+    private void getWeatherForCurrentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Clima", "Location changed");
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
 
+                RequestParams params = new RequestParams();
+                params.put("lat", latitude);
+                params.put("lon", longitude);
+                params.put("appid", APP_ID);
+
+                executeNetworkCall(params);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Log.d("Clima", "Location access has been turned off");
+            }
+        };
+
+        checkPermission();
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+    }
+
+    private void executeNetworkCall(RequestParams params) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(BASE_URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("Clima", "Request was a success!\n\n" + response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                Log.d("Clima", "Request failed: " + e.toString());
+                Toast.makeText(WeatherController.this, "Request failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Clima", "Permission granted");
+                getWeatherForCurrentLocation();
+            } else {
+                Log.d("Clima", "Permission denied");
+            }
+        }
+    }
 
     // TODO: Add getWeatherForNewCity(String city) here:
 
-
-
-    // TODO: Add getWeatherForCurrentLocation() here:
-
-
-
-    // TODO: Add letsDoSomeNetworking(RequestParams params) here:
-
-
-
     // TODO: Add updateUI() here:
 
-
-
     // TODO: Add onPause() here:
-
 
 
 }
